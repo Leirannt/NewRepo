c6a294dbcdf04834b4d2067ce2fa4c107a9e1409
MNFI13-blode <118786703+MNFI13-blode@users.noreply.github.com>
2023-04-05 17:29:59 +0700

    Menambahkan mahasiswa class

---
+++ DataMahasiswa_1302213131.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok_10
+{
+    public class DataMahasiswa_1302213131
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public void ReadJson()
+        {
+            string Data1 = File.ReadAllText(@"C:\Users\PRAKTIKAN\source\repos\modul7_kelompok_10\modul7_kelompok_10\jurnal7_1_1302213131.json");
+            DataMahasiswa_1302213131 mahasiswa = JsonSerializer.Deserialize<DataMahasiswa_1302213131>(Data1);
+            Console.WriteLine("Nama Awal  : " + mahasiswa.firstName);
+            Console.WriteLine("Nama Akhir : " + mahasiswa.lastName);
+            Console.WriteLine("Umur       : " + mahasiswa.age);
+            Console.WriteLine("Kelamin    : " + mahasiswa.gender);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using modul7_kelompok_10;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        DataMahasiswa_1302213131 nama = new DataMahasiswa_1302213131();
+        nama.ReadJson();
+    }
+}
